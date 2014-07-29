be6bfa23fe65ed49a2bed5b18dc424bde9cc925a
muneeb khan <munkhan33@gmail.com>
2014-07-30 00:15:29 +0500

    Uploaded the files.

---
+++ geome.js
@@ -0,0 +1,16 @@
+var app = new wilson();
+app.initapp("GeoMe", "wilson.geome", ['WQL']);
+	$.get("http://ipinfo.io", function(response) {
+  		 app.datastore("CHECK", response.country, null, null, website, function(data){
+  		 	console.log(data);
+  		 	if(data.exist == false){
+  		 		app.datastore("STORE", response.country, 1, "geomelocations", website, null);
+  		 	}else{
+  		 		app.datastore("GET", response.country, null, null, website, function(data){
+  		 			hits = data[0].data;
+  		 			hits++;
+  		 				app.datastore("UPDATE", response.country, hits, "geomelocations", website, null);
+  		 		});
+  		 	}
+  		 });
+	}, "jsonp");
