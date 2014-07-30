74b2ad083f35e3d9d6b71a693fde7c8ba3cb7a69
muneeb khan <munkhan33@gmail.com>
2014-07-30 23:18:22 +0500

    Changed index.html and geome.js
    Made minor changes to the wilson JS API so had to update these files.

--- geome.js
+++ geome.js
@@ -1,12 +1,11 @@
 var app = new wilson();
 app.initapp("GeoMe", "wilson.geome", ['datastore']);
 	$.get("http://ipinfo.io", function(response) {
-  		 app.datastore("CHECK", response.country, null, null, website, function(data){
-  		 	console.log(data);
+  		 app.datastore("CHECK", response.country, null, "geomelocations", website, function(data){
   		 	if(data.exist == false){
   		 		app.datastore("STORE", response.country, 1, "geomelocations", website, null);
   		 	}else{
-  		 		app.datastore("GET", response.country, null, null, website, function(data){
+  		 		app.datastore("GET", response.country, null, "geomelocations", website, function(data){
   		 			hits = data[0].data;
   		 			hits++;
   		 				app.datastore("UPDATE", response.country, hits, "geomelocations", website, null);
